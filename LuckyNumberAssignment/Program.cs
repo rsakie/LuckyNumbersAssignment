acb59da77db551bd39d411c2e7041b4d65183183
rsakie <rsakie@yahoo.com>
2017-07-07 16:27:36 -0400

    LuckyNumberAssignment

--- LuckyNumberAssignment/Program.cs
+++ LuckyNumberAssignment/Program.cs
@@ -11,22 +11,75 @@ namespace LuckyNumberAssignment
         static void Main(string[] args)
         {
 
+
+
             Console.WriteLine("Welcome to Lucky Numbers!!");
-            Console.WriteLine("To play you must input two numbers.");
-            Console.WriteLine("Please enter your first number, Make it a low Number.");
+
+            // Ask the user for a starting number for the lowest number in the number range.
+            // Ask the user for an ending number for the highest number in the number range.
+
+            Console.WriteLine("To play you must input two numbers.\n");
+            Console.WriteLine("Please enter your first number, Make sure its between 1 and 30 and very low.\n");
             int firstNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("NOw please enter a second number, But make sure there are atleast 30 numbers inbetween the 2 numbers you put in.");
+
+            Console.WriteLine("Now please enter a second number, But make it a really high one upto 30.\n");
+            int secNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now please enter 6 numbers");
+            //set my array
+            int[] luckyNumbers = new int[6];
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine("Please enter a number between 1 and 30");
+                luckyNumbers[i] = int.Parse(Console.ReadLine());
+                if (luckyNumbers[i] < firstNum || luckyNumbers[i] > secNum)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 30");
+                    break;
+                }
+                else if (firstNum < 1 || firstNum > 30)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 30");
+                    break;
+                }
+                if (secNum > 30 || secNum < 1)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 30");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
 
 
-          //  Ask the user for a starting number for the lowest number in the number range.
-          // Ask the user for an ending number for the highest number in the number range.
-          //Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
-          // Numbers must be stored in an array
-          // Array must be populated using a loop
-          //If the user enters a number that is outside of the range set,
-          //  prompt the user to give you a valid number. Do this until the user enters a valid number
+                //ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+                // Numbers must be stored in an array
+                // Array must be populated using a loop
+                //If the user enters a number that is outside of the range set,
+                //  prompt the user to give you a valid number. Do this until the user enters a valid number
+
+                //generate a  6 random numbers
+                //Random randomNumber = new Random(6);
+                //int numOne = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numOne);
+                //int numTwo = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numTwo);
+                //int numThree = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numThree);
+                //int numFour = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numFour);
+                //int numFive = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numFive);
+                //int numSix = randomNumber.Next(1, 30);
+                //Console.WriteLine("Lucky Number" + " " + numSix);
+                //int[] randomSix = { numOne, numTwo, numThree, numFour, numFive, numSix };
 
 
+
+            }
         }
     }
-}
+
+

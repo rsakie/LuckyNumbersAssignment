47fe5218a0aef711b93ee99655863c1d44990e0a
rsakie <rsakie@yahoo.com>
2017-07-07 13:19:46 -0400

    LuckyNumberAssignment

--- LuckyNumberAssignment/Program.cs
+++ LuckyNumberAssignment/Program.cs
@@ -10,6 +10,23 @@ namespace LuckyNumberAssignment
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to Lucky Numbers!!");
+            Console.WriteLine("To play you must input two numbers.");
+            Console.WriteLine("Please enter your first number, Make it a low Number.");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("NOw please enter a second number, But make sure there are atleast 30 numbers inbetween the 2 numbers you put in.");
+
+
+          //  Ask the user for a starting number for the lowest number in the number range.
+          // Ask the user for an ending number for the highest number in the number range.
+          //Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+          // Numbers must be stored in an array
+          // Array must be populated using a loop
+          //If the user enters a number that is outside of the range set,
+          //  prompt the user to give you a valid number. Do this until the user enters a valid number
+
+
         }
     }
 }
